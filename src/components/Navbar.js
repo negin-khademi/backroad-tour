f430b14af213ef8858b3e630a20f400c033b0a36
Negin <negin.khademi@gmail.com>
2024-12-05 14:31:47 +0330

    about ,hero and navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
+import { pageLinks, socialLinks } from '../data';
+
 import logo from '../images/logo.svg';
-import { pageLinks } from '../data';
 
 const Navbar = () => {
   return (
@@ -23,21 +24,16 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            // const { id, href, icon } = link;
+            return (
+              <li key={link.id}>
+                <a href={link.href} target="_blank" rel="noreferrer" className="nav-icon">
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
