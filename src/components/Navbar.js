85a1fe7ab04f889ff52ef4496e427b19647174b0
Negin <negin.khademi@gmail.com>
2024-12-05 13:49:46 +0330

    modify link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,13 @@ const Navbar = () => {
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
+          {pageLinks.map((link) => (
             <li key={link.id}>
               <a href={link.href} className="nav-link">
                 {link.text}
               </a>
-            </li>;
-          })}
+            </li>
+          ))}
         </ul>
 
         <ul className="nav-icons">
